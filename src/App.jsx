c1d908218fbfd65e12f4e1049283b85b2461223d
leminhhuywork <leminhhuy.work@gmail.com>
2024-10-30 20:07:26 +0700

    Finish Feature section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Features from "./components/Feature"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
@@ -12,6 +13,7 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
     </main>
   )
 }
